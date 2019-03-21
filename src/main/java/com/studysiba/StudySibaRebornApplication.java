e2be504bdf5b4f37777df12fa23f967f2d166c7e
bytrustu <bytrustu@gmail.com>
2019-03-21 19:02:41 +0900

    업데이트 : Database, Mybatis 연동, Log4j 설정

--- src/main/java/com/studysiba/StudySibaRebornApplication.java
+++ src/main/java/com/studysiba/StudySibaRebornApplication.java
@@ -1,13 +1,30 @@
 package com.studysiba;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
+@MapperScan("classpath:com.studysiba.mapper")
 public class StudySibaRebornApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudySibaRebornApplication.class, args);
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+        sqlSessionFactory.setDataSource(dataSource);
+        return sqlSessionFactory.getObject();
     }
 
 }
