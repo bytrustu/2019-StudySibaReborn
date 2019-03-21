461d1d3c9baad6db9bd3c5cb4377c8ef83443e33
bytrustu <bytrustu@gmail.com>
2019-03-21 18:24:06 +0900

    프로젝트 생성 : StudySibaReborn

---
+++ src/main/java/com/studysiba/StudySibaRebornApplication.java
@@ -0,0 +1,13 @@
+package com.studysiba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySibaRebornApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySibaRebornApplication.class, args);
+    }
+
+}
